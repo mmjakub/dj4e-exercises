54deeb9cb0a37ab0a6a71e93f627358396fd1bca
mmjakub <62809364+mmjakub@users.noreply.github.com>
2024-09-05 14:06:14 +0200

    use env vars in settings.py
    SECRET_KEY and DEBUG are expected in environment with DJANGO_ prefix

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-en*t&5qa=)aq3%cwyzhj4x%+u-a^um6hqy1!j(+)b#$3o#dbyx'
+try:
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find DJANGO_SECRET_KEY in environment")
+try:
+    DEBUG = os.environ["DJANGO_DEBUG"] == 'True'
+except KeyError as e:
+    DEBUG = False
+    print("Could not find DJANGO_DEBUG in environment, assuming DEBUG=False")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

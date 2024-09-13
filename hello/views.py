3dee437fb34855f7cc9e64c216c50f1c80113fb9
mmjakub <62809364+mmjakub@users.noreply.github.com>
2024-09-13 12:26:46 +0200

    assn(14) fix hello app to set cookie for autograder

--- hello/views.py
+++ hello/views.py
@@ -5,5 +5,6 @@ def sessfun(request) :
     request.session['num_visits'] = num_visits 
     if num_visits > 4 : del(request.session['num_visits'])
     resp = HttpResponse('view count='+str(num_visits))
+    resp.set_cookie('dj4e_cookie', '2b12fa2d', max_age=1000)
     return resp
 

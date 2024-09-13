c7a2b0e06ccbf78ef1c76d03993dd90c30b15277
mmjakub <62809364+mmjakub@users.noreply.github.com>
2024-09-13 12:21:41 +0200

    assn(14) refactor home urls
    urls are now included under 'home' namespace

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,13 +3,12 @@ import os
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
-from django.views.generic.base import TemplateView
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/main.html'), name='home'),
+    path('', include('home.urls')),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),

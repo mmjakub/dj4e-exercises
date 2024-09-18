1f15d5244a96ca4bdce00805e6851ec15938f935
mmjakub <62809364+mmjakub@users.noreply.github.com>
2024-09-18 13:34:38 +0200

    assn(18): copy provided files from dj4e-samples

--- autos/urls.py
+++ autos/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+from . import views
+
 app_name = 'autos'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='autos/index.html'),  name='index'),
+    path('', views.MainView.as_view(), name='all'),
+    path('main/create/', views.AutoCreate.as_view(), name='auto_create'),
+    path('main/<int:pk>/update/', views.AutoUpdate.as_view(), name='auto_update'),
+    path('main/<int:pk>/delete/', views.AutoDelete.as_view(), name='auto_delete'),
+    path('lookup/', views.MakeView.as_view(), name='make_list'),
+    path('lookup/create/', views.MakeCreate.as_view(), name='make_create'),
+    path('lookup/<int:pk>/update/', views.MakeUpdate.as_view(), name='make_update'),
+    path('lookup/<int:pk>/delete/', views.MakeDelete.as_view(), name='make_delete'),
 ]
+
+# Note that make_ and auto_ give us uniqueness within this application

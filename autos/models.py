b772d86201b86b2964d0b01c59d2fdbb1a66d3b8
mmjakub <62809364+mmjakub@users.noreply.github.com>
2024-09-18 14:02:50 +0200

    assn(18): add __str__ methods to models

--- autos/models.py
+++ autos/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Make(models.Model):
     name = models.CharField(max_length=80, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Auto(models.Model):
     nickname = models.CharField(max_length=80)
     make = models.ForeignKey(Make, on_delete=models.CASCADE)
     mileage = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     comments = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.nickname} ({self.make})'

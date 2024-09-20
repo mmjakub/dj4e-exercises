8d9e8e5536fa292f92f4c9a293df7cf6f47b9fde
mmjakub <62809364+mmjakub@users.noreply.github.com>
2024-09-20 12:17:48 +0200

    assn(19): implement cats CRUD
    passed manual testing, ready for autograder submition

--- cats/urls.py
+++ cats/urls.py
@@ -5,4 +5,11 @@ from cats import views as v
 app_name = 'cats'
 urlpatterns = [
     path('', v.CatListView.as_view(),  name='index'),
+    path('main/create', v.CatCreateView.as_view(), name='cat_create'),
+    path('main/<int:pk>/update', v.CatUpdateView.as_view(), name='cat_update'),
+    path('main/<int:pk>/delete', v.CatDeleteView.as_view(), name='cat_delete'),
+    path('lookup/', v.BreedListView.as_view(), name='breed_list'),
+    path('lookup/create/', v.BreedCreateView.as_view(), name='breed_create'),
+    path('lookup/<int:pk>/update', v.BreedUpdateView.as_view(), name='breed_update'),
+    path('lookup/<int:pk>/delete', v.BreedDeleteView.as_view(), name='breed_delete'),
 ]
--- cats/views.py
+++ cats/views.py
@@ -1,7 +1,45 @@
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from cats.models import Breed, Cat 
 
+class BreedListView(LoginRequiredMixin, ListView):
+    model = Breed
+
 class CatListView(LoginRequiredMixin, ListView):
     model = Cat
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['breed_count'] = len(Breed.objects.all())
+        return ctx
+
+class BreedMixin:
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:breed_list')
+
+class BreedCreateView(BreedMixin, LoginRequiredMixin, CreateView):
+    pass
+
+class BreedUpdateView(BreedMixin, LoginRequiredMixin, UpdateView):
+    pass
+
+class BreedDeleteView(BreedMixin, LoginRequiredMixin, DeleteView):
+    pass
+
+class CatMixin:
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:index')
+
+class CatCreateView(CatMixin, LoginRequiredMixin, CreateView):
+    pass
+
+class CatUpdateView(CatMixin, LoginRequiredMixin, UpdateView):
+    pass
+
+class CatDeleteView(CatMixin, LoginRequiredMixin, DeleteView):
+    pass
